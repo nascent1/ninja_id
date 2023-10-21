f49b719aab71bc48f92516e83007ac5dfdbc3c98
Peter <nascentmick@gmail.com>
2023-10-21 20:32:31 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: NinjaCard(),
+  ));
+}
+
+class NinjaCard extends StatefulWidget {
+  const NinjaCard({super.key});
+
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjaLevel = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: const Text("Ninja Id"),
+        backgroundColor: Colors.grey[850],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {
+          setState((){
+            ninjaLevel +=1;
+          })
+        },
+        backgroundColor: Colors.grey[800],
+        child: const Icon(Icons.add),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/img1.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            const Text(
+              'NAME',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'Chunli',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28.0),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            const Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              '$ninjaLevel',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28.0),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                const SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  "nas@nas.com",
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      letterSpacing: 1.0,
+                      color: Colors.grey[400]),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
